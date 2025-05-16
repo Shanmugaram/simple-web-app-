132d9aa539490e77f212852ce3b1f97613dbebee
shanmugaram <shanmugaram.aws@gmail.com>
2025-05-16 20:30:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on http://54.67.61.58:${PORT}`);
+});
